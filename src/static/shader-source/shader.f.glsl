c71921263b123f4930ccc300ea3e2e90420f610e
Artyom Belsky <artyombel2003@gmail.com>
2023-03-19 17:03:36 +0300

    Hp bar color is now changed adaptively.

--- src/static/shader-source/shader.f.glsl
+++ src/static/shader-source/shader.f.glsl
@@ -21,11 +21,21 @@ void main(void) {
     }
 }
 
+vec4 pickHPColor(float health) {
+    vec4 result = vec4(1.0, 1.0, 0.0, 1.0);
+    if (health >= 0.5) {
+        result.r = 1.0 - ((health - 0.5) / 0.5);
+    } else {
+        result.g = health / 0.5;
+    }
+    return result;
+}
+
 void drawHP (void) {
     float adaptedHpPerc = hpPerc * 2.0 - 1.0;
 
     if (pass_Pos.x < adaptedHpPerc) {
-        gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+        gl_FragColor = pickHPColor(hpPerc);
     } else {
         gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
     }
